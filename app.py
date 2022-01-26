e3ac226a996a392ff56cd80098810168dd6f8f6a
MatchHue <matthewpaul3358@gmail.com>
2022-01-26 22:11:05 +0000

    Model

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+db=SQLAlchemy(app)
+
+
+
+class Anime(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    anime = db.Column(db.String,nullable=False)
+    image = db.Column(db.String, nullable=False)
+
+    def toDict(self):
+      return {
+        "id": self.id,
+        "anime": self.anime,
+        "image": self.image,
+      }
+
+
+
 
 @app.route('/')
 def index():
-    return "Hello world"
+    return render_template('index.html')
 
 @app.route('/hello')
 def hello():
